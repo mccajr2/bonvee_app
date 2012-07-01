dab80d19fdaf9289751914f2c2898f6c380dd3a3
Jason McCarthy <mccarthy.jay@gmail.com>
2012-07-01 16:28:13 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BonveeApp::Application.config.secret_token = '27516f9db23f6b51a7c7a226e8a8d946e69fc9e757eb22b6bd2e488cfee07a0af25d73a76e7fdf9e6f270948e6692ab0195774d81b638c5cc50faf0fedc513c0'
